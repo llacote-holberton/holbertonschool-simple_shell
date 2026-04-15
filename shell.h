62253521e5896cc5203077111fefea7f179d464a
Laurent Lacôte <laurent.lacote@gmail.com>
2026-04-16 00:12:09 +0200

    Feat: declaring process_input publicly
    Will be useful at least for now to stress test
      its behaviour in some "test_main.c"
    Consider putting it in its own file
      instead of leaving it in main?

--- shell.h
+++ shell.h
@@ -1,6 +1,10 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+/* === PROTOTYPES - main.c === */
+int process_input(const char *received_input)
+/* @note: put here at least while we develop to allow "unit tests" */
+
 /* === PROTOTYPES - tokenizers.c === */
 /* Public facing function to get array of substrings from a string */
 char **tokenize_string(const char *string, char *delimiters);
