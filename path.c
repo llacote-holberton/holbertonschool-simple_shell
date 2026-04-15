3fb5d76b30fbd94caf9cad4382c3cd0213298dae
Souf-F <sfecom.31000@gmail.com>
2026-04-15 21:17:11 +0200

    Fix: Ensure command is executable in check_direct_path function

--- path.c
+++ path.c
@@ -36,7 +36,7 @@ static char *check_direct_path(char *command)
 
 	if (command[0] == '/' || (command[0] == '.' && command[1] == '/'))
 	{
-		if (stat(command, &st) == 0)
+		if (stat(command, &st) == 0 && (st.st_mode & S_IXUSR))
 			return (strdup(command));
 	}
 	return (NULL);
