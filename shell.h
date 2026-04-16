d03efa4c48de561b295303ae878f1885bebfb334
Laurent Lacôte <laurent.lacote@gmail.com>
2026-04-16 15:14:54 +0200

    Feat: implementing Command execution in subprocess.
    - Adds a prototype "execute_command" in header.
    - Implements it in executioner.c file.
    - Completes the process_input body by adding
      * search for command using path.c functions
      * call for execute_command if valid command found.

--- shell.h
+++ shell.h
@@ -2,12 +2,15 @@
 #define SHELL_H
 
 /* === PROTOTYPES - main.c === */
-int process_input(const char *received_input);
+int process_input(const char *received_input, char **envp);
 /* @note: put here at least while we develop to allow "unit tests" */
 
 /* === PROTOTYPES - tokenizers.c === */
 /* Public facing function to get array of substrings from a string */
 char **tokenize_string(const char *string, char *delimiters);
+
+/* === PROTOTYPES - executioner.c === */
+int execute_command(const char *command, char **arguments, char **envp);
 
 /* === INCLUDES === */
 #include <stdio.h>
