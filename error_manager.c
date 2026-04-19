a6e394bc3bc022c51c1547f58126b71d4e37e91a
Laurent Lacôte <laurent.lacote@gmail.com>
2026-04-19 14:36:44 +0200

    Error_Manager p02: Defining two distinct "channels" for errors
    Creating a distinction between "internal errors"
      (just for our own debug, not sure whether
       or not expected by checker)
    and "functional errors" (errors resulting from
      input not turning out to be an executable command
      OR command being run and returning its own errors).
    
    ADDED
    - Function to count the "lines" read from input,
      incremented through a call in main.
    - Function to report "internal errors" such as malloc
      or strdup failures (log_internal_error).
    - Function to report errors resulting from input
      process (log_functional_error).
    
    NOTE: functions will be simplified in next commit as
      in the end using sprintf is a useless middle step.
      We can just pass all arguments at once to fprintf.
    
    IMPORTANT: some functional errors will need to be implemented
      as custom messages hence probably one more function to write.

--- error_manager.c
+++ error_manager.c
@@ -27,3 +27,60 @@ char *get_set_program_name(char *string)
 	return ((program_name) ? program_name : "SHELL! ");
 }
 
+/**
+ * get_set_currentline_number - Returns the number of lines
+ *   read so far (including current).
+ * @increment: 0 means "read count", >0 means "increment by 1"
+ * Return: current number of lines read.
+ *
+ * NOTE: first name was get_set_inputlines_count but IMO
+ *   the new one is more adequate with intended use.
+ */
+unsigned int get_set_currentline_number(int increment)
+{
+	/* Static -> automatically initialized to 0. */
+	static unsigned int count;
+
+	if (increment)
+		count++;
+	return (count);
+}
+
+/**
+ * log_internal_error - Internal debugger (optional).
+ * @func_name: name of function from which error was triggered.
+ */
+void log_internal_error(char *func_name)
+{
+	/* @important: "shell_name: line_number: command provided"*/
+	char *prefix_tmpl = "%s: %d: %s";
+	static char msg_prefix[256];
+	char *name = NULL;
+	unsigned int line;
+
+	name = get_set_program_name(NULL);
+	line = get_set_currentline_number(0);
+	sprintf(msg_prefix, prefix_tmpl, name, line, func_name);
+
+	fprintf(stderr, "%s: %s\n", msg_prefix, strerror(errno));
+}
+
+/**
+ * log_functional_error - Wraps syscall error to give more info.
+ * @cmd_tried: what was tried to be run as command, from input.
+ */
+void log_functional_error(char *cmd_tried)
+{
+	/* @important: "shell_name: line_number: command provided"*/
+	char *prefix_tmpl = "%s: %d: %s";
+	static char msg_prefix[256];
+	char *name = NULL;
+	int line_number;
+
+	/* Preparing the "common part of all error messages". */
+	name = get_set_program_name(NULL);
+	line_number = get_set_currentline_number(0);
+	sprintf(msg_prefix, prefix_tmpl, name, line_number, cmd_tried);
+	/* Printing the complete error message on STDERR_NO*/
+	fprintf(stderr, "%s: %s\n", msg_prefix, strerror(errno));
+}
--- shell.h
+++ shell.h
@@ -15,6 +15,11 @@ int execute_command(const char *command, char **arguments, char **envp);
 
 /* === PROTOTYPES - error_manager.c*/
 char *get_set_program_name(char *string);
+unsigned int get_set_currentline_number(int increment);
+void log_internal_error(char *func_name);
+void log_functional_error(char *cmd_tried);
+
+
 void send_error_message(unsigned int code, char *function_name);
 
 
