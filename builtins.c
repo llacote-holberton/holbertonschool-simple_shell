ba1104b96946b26c444692ce47ac89263497162a
Mr.Robot <sfecom.31000@gmail.com>
2026-04-20 23:42:14 +0200

    Refactor builtin_exit and update execute_builtin
    Refactor builtin_exit to return exit code instead of calling exit directly. Update execute_builtin to handle the return value from builtin_exit.

--- builtins.c
+++ builtins.c
@@ -1,28 +1,22 @@
 #include "shell.h"
 
 /**
- * builtin_exit - Exits the shell
+ * builtin_exit - Signals shell should exit
  * @args: Array of command arguments
- * @line: Input line to free before exiting
+ * @line: Input line (unused here)
  *
- * Return: Does not return (calls exit)
+ * Return: Exit code (negative means exit requested)
  */
-void builtin_exit(char **args, char **line)
+int builtin_exit(char **args, char **line)
 {
 	int exit_code = 0;
 
-	/* Si exit a un argument, l'utiliser comme code de sortie */
-	if (args[1])
-	{
-		exit_code = atoi(args[1]);
-	}
+	(void)line;
 
-	if (line && *line)
-		free(*line);
-	if (args)
-		free(args);
-	
-	exit(exit_code);
+	if (args[1])
+		exit_code = atoi(args[1]);
+
+	return (-1 - exit_code);  /* -1 = exit(0), -2 = exit(1), etc. */
 }
 
 /**
@@ -50,17 +44,19 @@ void builtin_env(char **envp)
  * @envp: Environment variables
  * @line: Complete line to free if exit is called
  *
- * Return: 1 if a builtin was executed, 0 otherwise
+ * Return: 1 if builtin executed, <0 if exit, 0 otherwise
  */
 int execute_builtin(char **args, char **envp, char **line)
 {
+	int result;
+
 	if (!args || !args[0])
 		return (0);
 
 	if (strcmp(args[0], "exit") == 0)
 	{
-		builtin_exit(args, line);
-		return (1);
+		result = builtin_exit(args, line);
+		return (result);
 	}
 
 	if (strcmp(args[0], "env") == 0)
