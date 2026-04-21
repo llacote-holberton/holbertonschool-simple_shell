43da41843b83cbf8fa03d72030ea41b3d5e6685a
Mr.Robot <sfecom.31000@gmail.com>
2026-04-21 10:22:45 +0200

    Refactor: reorganize and restore function prototypes in shell.h

--- shell.h
+++ shell.h
@@ -1,19 +1,6 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-/* === PROTOTYPES - main.c === */
-int process_input(const char *received_input, char **envp, 
-									char *shell_name, int line_number);
-/* @note: put here at least while we develop to allow "unit tests" */
-
-/* === PROTOTYPES - tokenizers.c === */
-/* Public facing function to get array of substrings from a string */
-char **tokenize_string(const char *string, const char *delimiters,
-											 char **tokenized_string);
-
-/* === PROTOTYPES - executioner.c === */
-int execute_command(const char *command, char **arguments, char **envp);
-
 /* === INCLUDES === */
 #include <stdio.h>
 #include <stdlib.h>
@@ -23,16 +10,24 @@ int execute_command(const char *command, char **arguments, char **envp);
 #include <sys/wait.h>
 #include <sys/stat.h>
 
+/* === PROTOTYPES - main.c === */
+int process_input(const char *received_input, char **envp,
+									char *shell_name, int line_number);
+/* === PROTOTYPES - tokenizers.c === */
+/* Public facing function to get array of substrings from a string */
+char **tokenize_string(const char *string, const char *delimiters,
+											 char **tokenized_string);
+
+/* === PROTOTYPES - executioner.c === */
+int execute_command(const char *command, char **arguments, char **envp);
 
 /* === PROTOTYPES - path.c === */
 char *get_cmd_fullpath(char *command, char **envp);
 
-/* === PROTOTYPES - env_utils.c === */
+/* === PROTOTYPES - _getenv.c === */
 char *_getenv(const char *name, char **envp);
 
-/* === PROTOTYPES - main.c === */
-void free_array(char **array);
-
+/* === PROTOTYPES - builtins.c === */
 int execute_builtin(char **args, char **envp, char **line);
 
 #endif
